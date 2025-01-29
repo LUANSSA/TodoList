3677775ddb892d5b4bacc2c895c904009cdd99fb
Luan Souza <54681373+LUANSSA@users.noreply.github.com>
2025-01-29 11:48:44 -0300

    🎉 Commit Inicial:

---
+++ main.py
@@ -0,0 +1,23 @@
+import flet
+from flet import(
+    Checkbox,
+    Column,
+    FloatingActionButton,
+    IconButton,
+    OutlinedButton,
+    Page,
+    Row,
+    Tab,
+    Text,
+    TextField,
+    UserControl,
+    Colors,
+    Icons
+) # Importação dos componentes que vão ser usados na aplicação
+
+
+def main(page: Page):
+    pass
+
+
+flet.app(target=main)
